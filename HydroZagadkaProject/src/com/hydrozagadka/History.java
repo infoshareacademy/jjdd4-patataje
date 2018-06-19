5bbe4f6e4ea87c837d33a6541b1082ed861642aa
Pawel Orlikoski <orliktcz@gmail.com>
2018-06-19 16:51:45 +0200

    history repair

--- HydroZagadkaProject/src/com/hydrozagadka/History.java
+++ HydroZagadkaProject/src/com/hydrozagadka/History.java
@@ -8,16 +8,14 @@ public class History {
      Double waterDeep;
     private Double flow;
     private Double temperature;
-    private WaterContainer waterContainer;
 
-    public History(Integer year, Integer month, Integer day, Double waterDeep, Double flow, Double temperature, WaterContainer wc) {
+    public History(Integer year, Integer month, Integer day, Double waterDeep, Double flow, Double temperature) {
         this.year = year;
         this.month = month;
         this.day = day;
         this.waterDeep = waterDeep;
         this.flow = flow;
         this.temperature = temperature;
-        this.waterContainer = wc;
     }
 
 
--- HydroZagadkaProject/src/com/hydrozagadka/LoadFile.java
+++ HydroZagadkaProject/src/com/hydrozagadka/LoadFile.java
@@ -4,51 +4,52 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class LoadFile {
     BufferedReader br;
-    Map<Integer,WaterContainer> allContainers = new HashMap<>();
+    Map<Integer, WaterContainer> allContainers = new HashMap<>();
 
     LoadFile() throws FileNotFoundException {
-        br = new BufferedReader(new FileReader("/home/orzel/jjdd4-patataje/HydroZagadkaProject/plik.csv"));
+        br = new BufferedReader(new FileReader("plik.csv"));
     }
+
     public void load() throws IOException {
         String loadedLine;
         WaterContainer wc = null;
         String[] splitedLine;
-        List<History> helperList = new ArrayList<>();
-        while((loadedLine=br.readLine())!=null){
-            loadedLine = loadedLine.replaceAll("\"","");
+
+        while ((loadedLine = br.readLine()) != null) {
+            loadedLine = loadedLine.replaceAll("\"", "");
             splitedLine = loadedLine.split(",");
-            splitedLine[0]=splitedLine[0].trim();
-           if(!allContainers.containsKey(Integer.parseInt(splitedLine[0]))){
-               wc = new WaterContainer(Integer.parseInt(splitedLine[0]),splitedLine[1],splitedLine[2],helperList);
+            splitedLine[0] = splitedLine[0].trim();
 
+            if (!allContainers.containsKey(Integer.parseInt(splitedLine[0]))) {
+                wc = new WaterContainer(Integer.parseInt(splitedLine[0]), splitedLine[1], splitedLine[2], new ArrayList<>());
+                wc.history.add(new History(Integer.parseInt(splitedLine[3]), Integer.parseInt(splitedLine[4]),
+                        Integer.parseInt(splitedLine[5]), Double.parseDouble(splitedLine[6]), Double.parseDouble(splitedLine[7]), Double.parseDouble(splitedLine[8])));
 
-               allContainers.put(Integer.parseInt(splitedLine[0]),wc);
-
-               helperList.add(new History(Integer.parseInt(splitedLine[3]),Integer.parseInt(splitedLine[4]), 
-                       Integer.parseInt(splitedLine[5]),Double.parseDouble(splitedLine[6]),Double.parseDouble(splitedLine[7]),Double.parseDouble(splitedLine[8]),wc));
-           }else{
-               helperList.add(new History(Integer.parseInt(splitedLine[3]),Integer.parseInt(splitedLine[4]),
-                       Integer.parseInt(splitedLine[5]),Double.parseDouble(splitedLine[6]),Double.parseDouble(splitedLine[7]),Double.parseDouble(splitedLine[8]),wc));
-   //            helperList.removeAll(helperList);
-           }
+                allContainers.put(Integer.parseInt(splitedLine[0]), wc);
+            } else {
+                allContainers.get(Integer.parseInt(splitedLine[0])).history.add(new History(Integer.parseInt(splitedLine[3]), Integer.parseInt(splitedLine[4]),
+                        Integer.parseInt(splitedLine[5]), Double.parseDouble(splitedLine[6]), Double.parseDouble(splitedLine[7]), Double.parseDouble(splitedLine[8])));
+//                helperList.add(new History(Integer.parseInt(splitedLine[3]), Integer.parseInt(splitedLine[4]),
+//                        Integer.parseInt(splitedLine[5]), Double.parseDouble(splitedLine[6]), Double.parseDouble(splitedLine[7]), Double.parseDouble(splitedLine[8]), wc));
+                //            helperList.removeAll(helperList);
+            }
         }
     }
 
-    public void readExample(){
-           WaterContainer wt = allContainers.get(149180020);
-            System.out.println(wt.containerName+" ");
+    public void readExample() {
+        for (WaterContainer wt: allContainers.values()) {
+
+
+            System.out.println(wt.containerName + " ");
             System.out.println("==================");
-            for (History hs: wt.history) {
-                System.out.print(" "+hs.waterDeep);
+            for (History hs : wt.history) {
+                System.out.print(" " + hs.waterDeep);
             }
             System.out.println();
-
+        }
     }
 }
