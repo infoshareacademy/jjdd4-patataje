daa7afb8589bfd765333dbb40100fbd9b7035e99
Paulucha <pauluchp@gmail.com>
2018-06-25 22:53:28 +0200

    Terminal Menu Building in progress

--- HydroZagadkaProject/src/main/java/com/hydrozagadka/Province.java
+++ HydroZagadkaProject/src/main/java/com/hydrozagadka/Province.java
@@ -4,6 +4,7 @@ package com.hydrozagadka;
 import de.vandermeer.asciitable.AsciiTable;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
@@ -11,6 +12,7 @@ public class Province {
     private static List<String> province;
     private static CSVLoader csvLoader = new CSVLoader();
     private static FilterFiles filterFiles = new FilterFiles(csvLoader);
+    private static Map<Integer, WaterContainer> allFiles;
 
     public static void createMenu() {
         AsciiTable at = new AsciiTable();
@@ -38,6 +40,7 @@ public class Province {
         secmenu.addRule();
         secmenu.getContext().setWidth(70);
         System.out.println(secmenu.render());
+
     }
 
     public static void casemenu(Integer choice, Integer choice2) {
@@ -59,14 +62,56 @@ public class Province {
                 System.out.println(ac.render());
                 break;
             }
+            case 2:{
+                System.out.println("wybrales zbiornik");
+                break;
+            }
+            case 3: {
+                createMenu();
+                break;
+            }
         }
+    }
+
+    public static void showNewestData(int id) {
+        int lastIndexOfHistory = allFiles.get(id).getHistory().size() - 1;
+        AsciiTable sNd = new AsciiTable();
+        sNd.addRule();
+        sNd.addRow("NAZWA ZBIORNIKA", "NAZWA STACJI", "WOJEWÓDZTWO", "DATA", "STAN WODY", "PRZEPŁYW", "TEMPERATURA");
+        sNd.addRule();
+        sNd.addRow(allFiles.get(id).getContainerName(),
+                allFiles.get(id).getStationName(), allFiles.get(id).getProvince(),
+                allFiles.get(id).getHistory().get(lastIndexOfHistory).getDate(),
+                allFiles.get(id).getHistory().get(lastIndexOfHistory).getWaterDeep(),
+                allFiles.get(id).getHistory().get(lastIndexOfHistory).getFlow(),
+                allFiles.get(id).getHistory().get(lastIndexOfHistory).getTemperature());
+        System.out.println(sNd.render());
+    }
+
+    public static void showHistoricData(int id) {
+        WaterContainer wt = allFiles.get(id);
+
+        AsciiTable sHd = new AsciiTable();
+        sHd.addRule();
+        sHd.addRow("STACJA: ", wt.getContainerName(), wt.getProvince(), wt.getStationName());
+
+        sHd.addRule();
+        sHd.addRow("DATA", "STAN WODY", "PRZEPŁYW", "TEMPERATURA");
+        sHd.addRule();
+        wt.getHistory().stream().sorted((o1, o2) -> o2.getDate().compareTo(o1.getDate())).forEach(hs -> {
+            sHd.addRow(hs.getDate(), hs.getWaterDeep(), hs.getFlow(), hs.getTemperature());
+            sHd.addRule();
+        });
+        System.out.println(sHd.render());
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         province = csvLoader.getProvince().stream().collect(Collectors.toList());
+        Integer choice2 = -1;
         createMenu();
         Integer choice = -1;
+
         while (choice != 0) {
 
             String keyboardChoice = scanner.next();
@@ -78,12 +123,15 @@ public class Province {
             if (choice <= 16 && choice >= 1) {
                 secondMenu();
                 try {
-                    Integer choice2 = scanner.nextInt();
+                    choice2 = scanner.nextInt();
                     casemenu(choice, choice2);
 
                 } catch (Exception e) {
                     System.out.println("Podales niewlasciwy znak!");
                 }
+            }
+            if (choice2 ==1 ){
+
             }
         }
     }
