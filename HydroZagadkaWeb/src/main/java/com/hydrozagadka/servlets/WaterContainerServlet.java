59286a7fdab7cb0987b8d1c26971a1ac9f16b6e7
jkloss <j.klos0405@gmail.com>
2018-07-17 22:02:34 +0200

    Added doGet in order to check if it works

--- HydroZagadkaWeb/src/main/java/com/hydrozagadka/servlets/WaterContainerServlet.java
+++ HydroZagadkaWeb/src/main/java/com/hydrozagadka/servlets/WaterContainerServlet.java
@@ -1,5 +1,6 @@
 package com.hydrozagadka.servlets;
 
+import com.hydrozagadka.History;
 import com.hydrozagadka.WaterContainer;
 import com.hydrozagadka.dao.WaterContainerDao;
 
@@ -10,6 +11,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 @WebServlet(urlPatterns = "/waterContainer")
@@ -19,12 +22,22 @@ public class WaterContainerServlet extends HttpServlet {
     private WaterContainerDao waterContainerDao;
 
     @Override
+    public void init() throws ServletException {
+        List<History> list = new ArrayList<>();
+        WaterContainer waterContainer = new WaterContainer(234L, "dwdwd", "dwd", "dwada", list);
+        waterContainerDao.save(waterContainer);
+    }
+
+    @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         final String action = req.getParameter("action");
-//        if ()
-//        if () {
-//            findAllWaterContainers(req, resp);
-//        }
+         if (action == null || action.isEmpty()) {
+             resp.getWriter().write("Complete parameter");
+             return;
+         }
+           if (action.equals("findAllContainers")) {
+            findAllWaterContainers(req, resp);
+        }
     }
 
     private void findAllWaterContainers(HttpServletRequest req, HttpServletResponse resp) throws IOException {
