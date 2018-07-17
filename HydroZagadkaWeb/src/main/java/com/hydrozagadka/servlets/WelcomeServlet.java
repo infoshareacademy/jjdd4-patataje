47c9a74f99f2ab85f8b2a8bdfb0dc83dfb3549fb
Paulina Przekop <paulina.przekop@gmail.com>
2018-07-17 15:59:41 +0200

    freemarker templates created

--- HydroZagadkaWeb/src/main/java/com/hydrozagadka/servlets/WelcomeServlet.java
+++ HydroZagadkaWeb/src/main/java/com/hydrozagadka/servlets/WelcomeServlet.java
@@ -22,39 +22,27 @@ import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-@WebServlet("/")
+@WebServlet("/welcome")
 public class WelcomeServlet extends HttpServlet {
 
     @Inject
     FreeMarkerConfig freeMarkerConfig;
 
-    @Inject
-    ProvinceBean provinceBean;
-
-    private CSVLoader load;
-    private FilterFiles ff;
-
-    @Override
-    public void init() throws ServletException {
-        super.init();
-         load = new CSVLoader();
-         ff = new FilterFiles(load);
-    }
 
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        PrintWriter pr = response.getWriter();
+
+        Template template = freeMarkerConfig.getTemplate("index.ftlh", getServletContext());
+
         Map<String, Object> model = new HashMap<>();
-        model.put("provinces", load.getProvince());
-        Template template = freeMarkerConfig.getTemplate("firstMenu.ftlh", getServletContext());
+
         try {
-            template.process(model, pr);
+            template.process(model, response.getWriter());
         } catch (TemplateException e) {
             e.printStackTrace();
         }
-        pr.close();
     }
 }
