93a3ad405a51ccfad791a21153206e70120d80a9
Juliusz Klos <j.klos0405@gmail.com>
2018-07-18 16:55:41 +0200

    CSV loading fixed

--- HydroZagadkaApp/src/main/java/com/hydrozagadka/WaterContainer.java
+++ HydroZagadkaApp/src/main/java/com/hydrozagadka/WaterContainer.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class WaterContainer {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    //@GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
     @Column(name = "container_name")
