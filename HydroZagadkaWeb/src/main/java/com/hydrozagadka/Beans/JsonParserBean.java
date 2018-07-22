18f063b736e232e28a84454b199e540d4b72d877
orzeltcz <orlikcz@gmail.com>
2018-07-22 21:20:01 +0200

    bean parses added

--- HydroZagadkaWeb/src/main/java/com/hydrozagadka/Beans/JsonParserBean.java
+++ HydroZagadkaWeb/src/main/java/com/hydrozagadka/Beans/JsonParserBean.java
@@ -5,26 +5,15 @@ import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import com.hydrozagadka.History;
-import com.hydrozagadka.dao.HistoryDao;
-import com.hydrozagadka.dao.WaterContainerDao;
 
 import javax.enterprise.context.Dependent;
-import javax.enterprise.context.RequestScoped;
-import javax.inject.Inject;
 import java.util.List;
-@RequestScoped
+@Dependent
 public class JsonParserBean<T>  {
 
     private ObjectMapper objectMapper = new ObjectMapper();
 
-    @Inject
-    private HistoryDao historyDao;
 
-    @Inject
-   private WaterContainerDao waterContainerDao;
-
-    List<History> histories;
 
     public String parseToJson(List<T> dataToParse) throws JsonProcessingException {
         objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
