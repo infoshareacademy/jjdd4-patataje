9f1764e35db6c3a603dffe131dbf9e38dcff92a7
Pawel Orlikoski <orliktcz@gmail.com>
2018-07-30 10:02:33 +0200

    map precision changed

--- HydroZagadkaWeb/src/main/webapp/js/formcreator.js
+++ HydroZagadkaWeb/src/main/webapp/js/formcreator.js
@@ -44,7 +44,7 @@ $(document).ready(function () {
         if($("#province").val()==-1 || $("#watercontainer").val()==-1){
             return;
         }
-        var rzeka=$("#watercontainer").val();
+
 
         $('.invalid-feedback').css("display","none");
         $.ajax({
@@ -66,54 +66,7 @@ $(document).ready(function () {
             }
         );
 
-        drawMap()
 
-        function drawMap() {
-            var data = new google.visualization.DataTable();
-            data.addColumn('string', 'Address');
-            data.addColumn('string', 'Location');
-            data.addRows([
-                ['rzeka '+rzeka, rzeka]
-
-            ]);
-
-            var options = {
-                mapType: 'styledMap',
-                zoomLevel: 9,
-                showTooltip: true,
-                showInfoWindow: true,
-                useMapTypeControl: true,
-                maps: {
-                    // Your custom mapTypeId holding custom map styles.
-                    styledMap: {
-                        name: 'Styled Map', // This name will be displayed in the map type control.
-                        styles: [
-                            {
-                                featureType: 'poi.attraction',
-                                stylers: [{color: '#fce8b2'}]
-                            },
-                            {
-                                featureType: 'road.highway',
-                                stylers: [{hue: '#0277bd'}, {saturation: -50}]
-                            },
-                            {
-                                featureType: 'road.highway',
-                                elementType: 'labels.icon',
-                                stylers: [{hue: '#000'}, {saturation: 100}, {lightness: 50}]
-                            },
-                            {
-                                featureType: 'landscape',
-                                stylers: [{hue: '#259b24'}, {saturation: 10}, {lightness: -22}]
-                            }
-                        ]
-                    }
-                }
-            };
-
-            var map = new google.visualization.Map(document.getElementById('map_div'));
-            map.draw(data, options);
-            $('#mapa').fadeIn(2000);
-        }
     })
 
 
@@ -147,6 +100,58 @@ $(document).ready(function () {
 
 
                         drawChart()
+
+                        var rzeka=$("#watercontainer").val();
+                        var selectedOptionText = $('#station option:selected').text()
+
+                        drawMap(selectedOptionText)
+
+                        function drawMap(name) {
+                            var data = new google.visualization.DataTable();
+                            data.addColumn('string', 'Address');
+                            data.addColumn('string', 'Location');
+                            data.addRows([
+                                ['rzeka '+ rzeka + ' ' + name, rzeka + ' ' + name]
+
+                            ]);
+
+                            var options = {
+                                mapType: 'styledMap',
+                                zoomLevel: 9,
+                                showTooltip: true,
+                                showInfoWindow: true,
+                                useMapTypeControl: true,
+                                maps: {
+                                    // Your custom mapTypeId holding custom map styles.
+                                    styledMap: {
+                                        name: 'Styled Map', // This name will be displayed in the map type control.
+                                        styles: [
+                                            {
+                                                featureType: 'poi.attraction',
+                                                stylers: [{color: '#fce8b2'}]
+                                            },
+                                            {
+                                                featureType: 'road.highway',
+                                                stylers: [{hue: '#0277bd'}, {saturation: -50}]
+                                            },
+                                            {
+                                                featureType: 'road.highway',
+                                                elementType: 'labels.icon',
+                                                stylers: [{hue: '#000'}, {saturation: 100}, {lightness: 50}]
+                                            },
+                                            {
+                                                featureType: 'landscape',
+                                                stylers: [{hue: '#259b24'}, {saturation: 10}, {lightness: -22}]
+                                            }
+                                        ]
+                                    }
+                                }
+                            };
+
+                            var map = new google.visualization.Map(document.getElementById('map_div'));
+                            map.draw(data, options);
+                            $('#mapa').fadeIn(2000);
+                        }
                         function drawChart() {
                             var chartDiv = document.getElementById('curve_chart');
                            var data = new google.visualization.DataTable();
