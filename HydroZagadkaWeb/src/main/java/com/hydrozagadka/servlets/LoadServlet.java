5b35e84f1ffc3d2a9c7ed2e0d0eed7f688228466
Paulina Przekop <paulina.przekop@gmail.com>
2018-07-30 16:05:41 +0200

    path changed

--- HydroZagadkaWeb/src/main/java/com/hydrozagadka/servlets/LoadServlet.java
+++ HydroZagadkaWeb/src/main/java/com/hydrozagadka/servlets/LoadServlet.java
@@ -18,11 +18,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.PrintWriter;
+import java.io.*;
+import java.net.URI;
 import java.nio.file.Paths;
 import java.util.HashMap;
+import java.util.Properties;
 import java.util.logging.LogManager;
 import java.util.Map;
 
@@ -39,7 +39,9 @@ public class LoadServlet extends HttpServlet {
 
     private Map<Long, WaterContainer> waterContainerMap;
 
-    public static final String DIRECT_PATH = "../../../../../HydroZagadkaApp/data";
+    public static final String DIRECT_PATH = System.getProperty("java.io.tmpdir");
+
+
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         Part filePart = request.getPart("file");
