cbb0178438a5303b79d441a2c32c24fd08c053c5
Lucjan Wroblewski <lucjan.wroblewski@gmail.com>
2018-08-01 15:44:18 +0200

    NewestHistoryDataLoadBean unit test added

---
+++ HydroZagadkaWeb/src/test/com/hydrozagadka/Beans/DatabaseLoadBeanTest.java
@@ -0,0 +1,26 @@
+package com.hydrozagadka.Beans;
+
+import com.hydrozagadka.CSVLoader;
+import com.hydrozagadka.WaterContainer;
+import org.junit.Test;
+import org.mockito.Mock;
+
+
+import static org.junit.Assert.*;
+
+public class DatabaseLoadBeanTest {
+    @Mock
+    private CSVLoader csvLoader;
+
+    @Test
+    public void loadWaterContainer() {
+    }
+
+    @Test
+    public void loadHistory() {
+    }
+
+    @Test
+    public void loadDataFromApi() {
+    }
+}
